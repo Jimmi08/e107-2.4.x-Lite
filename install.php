dd642809d552da17943875bebfed7a2c5a09a4ad
e107help <help@e107.org>
2026-07-23 00:33:55 +0200

    fix(install): report the real problem when MySQL is below the minimum version
    Cherry-picked from upstream e107inc/e107 dd49fc46fecfa5aa733ece6cf7e774f6b568f3d7.
    Path remapped e107_languages/ -> elanguages/.

--- install.php
+++ install.php
@@ -1390,7 +1390,7 @@ return [
 			}
 			else
 			{
-				$mysql_help = "<span class='glyphicon glyphicon-remove'></span> ".LANINS_105;
+				$mysql_help = "<span class='glyphicon glyphicon-remove'></span> ".str_replace('[x]', MIN_MYSQL_VERSION, LANINS_150);
 			}
 		}
 
